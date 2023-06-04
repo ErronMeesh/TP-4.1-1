22a5201fd8bec60b4a547275f9872043cc4e0a97
ErronMeesh <D.E.F.A.U.L.T-2@yandex.ru>
2023-06-04 23:59:56 +0300

    add fullstack
    add fullstack

---
+++ FullStack/Server/src/main/java/backend/controller/FormQrController.java
@@ -0,0 +1,55 @@
+package backend.controller;
+
+import backend.model.Flight;
+import backend.model.HistoryOfOrders;
+import backend.model.Passenger;
+import backend.model.User;
+import backend.service.FlightService;
+import backend.service.HistoryOfOrdersService;
+import backend.service.UserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@Api
+
+@RequestMapping("")
+public class FormQrController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private FlightService flightService;
+    @Autowired
+    private HistoryOfOrdersService historyOfOrdersService;
+
+
+    @GetMapping("/FormQr")
+
+    public String testAddControllerMethod_GETMAPPING(Principal principal, Model model) {
+
+        boolean authorized = principal != null;
+
+        model.addAttribute("authorized", authorized);
+        if (authorized) {
+
+            User user = userService.getByLogin(principal.getName());
+            model.addAttribute("user", user);
+        }
+
+        List<Flight> destP = flightService.getAll();
+
+        model.addAttribute("destP", destP);
+
+        return "ready-html/formQr.html";
+    }
+}
